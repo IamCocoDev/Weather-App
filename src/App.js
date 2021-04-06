70f4003ca24569b665d60c37d6429d00056f2406
Federico Giovenco <giovencofede@gmail.com>
2021-04-06 14:14:37 -0300

    Agregue todo

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   const [cities, setCities] = useState([]);
 
   function onSearch(ciudad) {
-    const apiKey = '4ae2636d8dfbdc3044bede63951a019b'
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Logo from '../logoHenry.png'
 import SearchBar from './SearchBar.jsx'; 
 import './Nav.css';
 
 function Nav({onSearch}) {
   return (
     <nav className = {"navBar"}>
-      <div className = {"logoHenry"}>
-        <img src={Logo} alt="Logo de Henry"/>
+      <div>
       </div>
       <SearchBar onSearch={onSearch}/>    
     </nav>
